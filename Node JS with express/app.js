003dbe625044a016eb8c521bd4b3eb3c7ef7d5ca
shaiJain0894 <shailendrajain.javaventures@gmail.com>
2024-03-21 15:21:03 -0400

    first get request with express

--- Node JS with express/app.js
+++ Node JS with express/app.js
@@ -1,18 +1,28 @@
 // import express package 
 const express = require('express');
 const app = express();
+const fs = require('fs')
 
 //Routes = http method + url
 
 // for get request
-app.get('/', (req, res) => {
-    // res.status(200).send('<h4>Hello from express server</h4>')  // for html file
-    res.status(200).json({message:'Hello World', status:200})
-})
+// app.get('/', (req, res) => {
+//     // res.status(200).send('<h4>Hello from express server</h4>')  // for html file
+//     res.status(200).json({message:'Hello World', status:200})
+// })
 
-// for post request
-app.post('/', () => { 
+let movies = JSON.parse(fs.readFileSync('./data/movies.json'));
 
+// Get - api/movies
+
+app.get('/api/v1/movies', (req,res) => {
+    res.status(200).json({
+        status:"Success",
+        count:movies.length,
+        data: {
+            movies: movies
+        }
+    })
 })
 
 // create a server 
