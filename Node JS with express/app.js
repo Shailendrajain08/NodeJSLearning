430d0bf9aa68b39ba1cf98cc81e93ee25ec4d58b
shaiJain0894 <shailendrajain.javaventures@gmail.com>
2024-03-21 15:42:33 -0400

    first post request with express

--- Node JS with express/app.js
+++ Node JS with express/app.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const fs = require('fs')
 
+let movies = JSON.parse(fs.readFileSync('./data/movies.json'));
+app.use(express.json())
+
+
 //Routes = http method + url
 
 // for get request
@@ -11,10 +15,9 @@ const fs = require('fs')
 //     res.status(200).json({message:'Hello World', status:200})
 // })
 
-let movies = JSON.parse(fs.readFileSync('./data/movies.json'));
+
 
 // Get - api/movies
-
 app.get('/api/v1/movies', (req,res) => {
     res.status(200).json({
         status:"Success",
@@ -23,7 +26,24 @@ app.get('/api/v1/movies', (req,res) => {
             movies: movies
         }
     })
-})
+});
+
+// Post - api/movies
+app.post('/api/v1/movies', (req,res) => {
+    // console.log(req.body)
+    const newId = movies[movies.length - 1].id + 1;
+    const newMovie = Object.assign({id: newId}, req.body);
+    movies.push(newMovie)
+    fs.writeFile('./data/movies.json', JSON.stringify(movies), (err) => {
+        res.status(201).json({
+            status : 'success',
+            data: {
+                movie : newMovie
+            }
+        })
+    })
+});
+
 
 // create a server 
 const port = 3000;
